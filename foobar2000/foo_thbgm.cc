6b838715ac842c21e21fc96266e79f783f4de06e
otoboku <otoboku@users.noreply.github.com>
2015-11-29 16:18:17 +0800

    2012/05/27 17:06 解决wav加载播放时间问题 加载其它列表改变loop参数

--- foobar2000/foo_thbgm.cc
+++ foobar2000/foo_thbgm.cc
@@ -16,6 +16,8 @@ static cfg_bool loopforever(cfg_loop_forever, true);
 static cfg_uint loopcount(cfg_loop_count, 1);
 static cfg_bool read_thbgm_info(cfg_thbgm_readinfo, false);
 
+PFC_DECLARE_EXCEPTION(exception_loop_out_of_range,pfc::exception,"error:loop out of range")
+
 const t_uint32 deltaread = 1024;
 //double seek_seconds;
 t_uint32 samplerate;
@@ -130,6 +132,7 @@ private:
 	input_decoder::ptr decoder;
 	bool first_packet;
 	t_filesize current_sample;
+	t_filesize read_count;
 	double seek_seconds;
 public:
 	void open(const char *p_path, t_input_open_reason p_reason,
@@ -172,14 +175,17 @@ public:
 		}
 		
 		bool result = decoder->run(p_chunk, p_abort);
-		if(!needloop) {
-			return result;
-		} else {
-			if(current_sample > m_totallen) return false; //Decoding failure at xxx (bad allocation)
-			t_size read_count = p_chunk.get_sample_count();
-			current_sample += read_count;
+		read_count = p_chunk.get_sample_count();
+		current_sample += read_count;
+
+		if(needloop) {
+			if(current_sample > m_totallen + read_count) {
+				throw exception_loop_out_of_range();
+				return false; //Decoding failure at xxx (bad allocation)
+			}
+
 			if(current_sample >= m_totallen || !result) {
-				double looptime = audio_math::samples_to_time(m_headlen, samplerate);
+				//double looptime = audio_math::samples_to_time(m_headlen, samplerate);
 				truncate_chunk(p_chunk, read_count  - (current_sample - m_totallen));
 				current_sample = m_headlen;
 				decoder->seek(audio_math::samples_to_time(m_offset + m_headlen, samplerate), p_abort);
@@ -187,8 +193,8 @@ public:
 				if(!loopforever) current_loop++;
 				//return run(p_chunk, p_abort);
 			}
-			return result;
 		}
+		return result;
 	}
 
 	void seek(double seconds) {
@@ -219,12 +225,12 @@ protected:
 	input_raw raw;
 	pfc::array_t<t_uint8> m_buffer;
 
-	inline t_uint64 length_to_samples(t_filesize p_length) {
-		return p_length / samplewidth;
+	inline t_uint64 length_to_samples(t_filesize p_length, t_uint32 p_samplewidth) {
+		return p_length / p_samplewidth;
 	}
 
-	inline t_filesize samples_to_length(t_uint64 p_samples) {
-		return p_samples * samplewidth;
+	inline t_filesize samples_to_length(t_uint64 p_samples, t_uint32 p_samplewidth) {
+		return p_samples * p_samplewidth;
 	}
 
 	void open_raw(t_uint32 p_subsong, abort_callback &p_abort) {
@@ -328,27 +334,8 @@ public:
 	}
 
 	t_uint32 get_subsong(t_uint32 p_subsong) {
-		isWave = bgmlist[++p_subsong]["codec"] == "PCM" && !isArchive;
-		codec = bgmlist[p_subsong]["codec"].c_str();
-		encoding = bgmlist[p_subsong]["encoding"].c_str();
-		title = bgmlist[p_subsong]["title"].c_str();
-		artist = bgmlist[p_subsong]["artist"].c_str();
-
-		string pos = bgmlist[p_subsong]["pos"];
-		size_t pos_head = pos.find(',');
-		size_t head_loop = pos.rfind(',');
-		m_offset = _atoi64(pos.substr(0, pos_head).c_str());
-		m_headlen = _atoi64(pos.substr(++pos_head, head_loop).c_str());
-		m_looplen = _atoi64(pos.substr(++head_loop).c_str());
-		m_totallen = m_headlen + m_looplen;
-
-		samplerate = atoi(bgmlist[p_subsong]["samplerate"].c_str());
-		bits = atoi(bgmlist[p_subsong]["bits"].c_str());
-		channels = atoi(bgmlist[p_subsong]["channels"].c_str());
-		samplewidth = bits * channels / 8;
-
-		assert(p_subsong < get_subsong_count() + 1);
-		return p_subsong;
+		assert(p_subsong < get_subsong_count());
+		return p_subsong + 1;
 	}
 
 	void get_info(t_uint32 p_subsong, file_info &p_info,
@@ -409,7 +396,7 @@ public:
 		if(isWave) {
 			p_info.info_set_bitrate((bits * channels * samplerate + 500) / 1000);
 			p_info.set_length(audio_math::samples_to_time(
-				length_to_samples(m_headlen + m_looplen), samplerate));
+				length_to_samples(m_headlen + m_looplen, samplewidth), samplerate));
 		} else {
 			p_info.set_length(audio_math::samples_to_time(
 				m_headlen + m_looplen, samplerate));
@@ -438,9 +425,28 @@ public:
 
 	void decode_initialize(t_uint32 p_subsong, unsigned p_flags,
 												abort_callback &p_abort) {
+		isWave = bgmlist[p_subsong]["codec"] == "PCM" && !isArchive;
+		codec = bgmlist[p_subsong]["codec"].c_str();
+		encoding = bgmlist[p_subsong]["encoding"].c_str();
+		title = bgmlist[p_subsong]["title"].c_str();
+		artist = bgmlist[p_subsong]["artist"].c_str();
+
+		string pos = bgmlist[p_subsong]["pos"];
+		size_t pos_head = pos.find(',');
+		size_t head_loop = pos.rfind(',');
+		m_offset = _atoi64(pos.substr(0, pos_head).c_str());
+		m_headlen = _atoi64(pos.substr(++pos_head, head_loop).c_str());
+		m_looplen = _atoi64(pos.substr(++head_loop).c_str());
+		m_totallen = m_headlen + m_looplen;
+
+		samplerate = atoi(bgmlist[p_subsong]["samplerate"].c_str());
+		bits = atoi(bgmlist[p_subsong]["bits"].c_str());
+		channels = atoi(bgmlist[p_subsong]["channels"].c_str());
+		samplewidth = bits * channels / 8;
+
 		open_raw(get_subsong(--p_subsong), p_abort);
 		if(isWave) {
-			m_buffer.set_size(samples_to_length(deltaread));
+			m_buffer.set_size(samples_to_length(deltaread, samplewidth));
 		}
 		current_loop = 1;
 		decode_seek(0, p_abort);
@@ -450,7 +456,7 @@ public:
 		if(isWave) {
 			if(m_filepos >= m_maxseeksize) return false; 
 			t_size deltaread_size = pfc::downcast_guarded<t_size>
-				(pfc::min_t(samples_to_length(deltaread), m_maxseeksize - m_filepos));
+				(pfc::min_t(samples_to_length(deltaread, samplewidth), m_maxseeksize - m_filepos));
 			t_size deltaread_done = 
 				raw.read(m_buffer.get_ptr(), deltaread_size, p_abort);
 			m_filepos += deltaread_done;
